f4e3096b583a15175a9cc5a068d987bc30f0f717
kamei <leokamei1@gmail.com>
2025-03-15 22:47:41 -0300

    cohen implemented!

--- main_window.py
+++ main_window.py
@@ -21,6 +21,7 @@ class MainWindow(QMainWindow):
         menu_bar = self.menuBar()
         transform_menu = menu_bar.addMenu("Transformações")
         shapes_menu = menu_bar.addMenu("Formas")
+        clipping_menu = menu_bar.addMenu("Recorte")
 
         criation_action = QAction("Criação", self)
         translate_action = QAction("Translação", self)
@@ -31,6 +32,9 @@ class MainWindow(QMainWindow):
         line_action = QAction("Reta", self)
         polygon_action = QAction("Polígono", self)
 
+        cohen_action = QAction("Cohen", self)
+        exit_clipping_action = QAction("Sair do modo de recorte", self)
+
         line_action.triggered.connect(lambda: self.set_shape_type("line"))
         circle_action.triggered.connect(lambda: self.set_shape_type("circle"))
         polygon_action.triggered.connect(lambda: self.set_shape_type("polygon"))
@@ -40,6 +44,9 @@ class MainWindow(QMainWindow):
         rotate_action.triggered.connect(lambda: self.set_transformation_mode("rotate"))
         scale_action.triggered.connect(lambda: self.set_transformation_mode("scale"))
         
+        cohen_action.triggered.connect(lambda: self.set_clipping_mode("cohen"))
+        exit_clipping_action.triggered.connect(lambda: self.set_clipping_mode(None))
+
         transform_menu.addAction(criation_action)
         transform_menu.addAction(translate_action)
         transform_menu.addAction(rotate_action)
@@ -49,23 +56,100 @@ class MainWindow(QMainWindow):
         shapes_menu.addAction(circle_action)
         shapes_menu.addAction(polygon_action)
 
+        clipping_menu.addAction(cohen_action)
+        clipping_menu.addAction(exit_clipping_action)
+
     def set_transformation_mode(self, mode):
         self.drawing_area.transformation_mode = mode
+        self.drawing_area.clipping_mode = None
+    
+    def set_clipping_mode(self, mode):
+        if(mode == None):
+            self.drawing_area.clip_window = None
+        self.drawing_area.clipping_mode = mode
+        self.drawing_area.transformation_mode = None
 
     def set_shape_type(self, shape_type):
         self.drawing_area.shape_type = shape_type
 
+class CohenSutherland:
+    INSIDE = 0
+    LEFT = 1
+    RIGHT = 2
+    BOTTOM = 4
+    TOP = 8
+
+    def __init__(self, x_min, y_min, x_max, y_max):
+        self.x_min = x_min
+        self.y_min = y_min
+        self.x_max = x_max
+        self.y_max = y_max
+
+    def compute_code(self, x, y):
+        code = self.INSIDE
+        if x < self.x_min:
+            code |= self.LEFT
+        elif x > self.x_max:
+            code |= self.RIGHT
+        if y < self.y_min:
+            code |= self.BOTTOM
+        elif y > self.y_max:
+            code |= self.TOP
+        return code
+
+    def clip_line(self, line):
+        x1, y1 = line.start_point.x(), line.start_point.y()
+        x2, y2 = line.end_point.x(), line.end_point.y()
+        code1 = self.compute_code(x1, y1)
+        code2 = self.compute_code(x2, y2)
+        accept = False
+
+        while True:
+            if code1 == 0 and code2 == 0:
+                accept = True
+                break
+            elif (code1 & code2) != 0:
+                break
+            else:
+                x, y = 0, 0
+                out_code = code1 if code1 != 0 else code2
+                if out_code & self.TOP:
+                    x = x1 + (x2 - x1) * (self.y_max - y1) / (y2 - y1)
+                    y = self.y_max
+                elif out_code & self.BOTTOM:
+                    x = x1 + (x2 - x1) * (self.y_min - y1) / (y2 - y1)
+                    y = self.y_min
+                elif out_code & self.RIGHT:
+                    y = y1 + (y2 - y1) * (self.x_max - x1) / (x2 - x1)
+                    x = self.x_max
+                elif out_code & self.LEFT:
+                    y = y1 + (y2 - y1) * (self.x_min - x1) / (x2 - x1)
+                    x = self.x_min
+                
+                if out_code == code1:
+                    x1, y1 = x, y
+                    code1 = self.compute_code(x1, y1)
+                else:
+                    x2, y2 = x, y
+                    code2 = self.compute_code(x2, y2)
+        
+        if accept:
+            return Line(QPoint(int(x1), int(y1)), QPoint(int(x2), int(y2)))
+        return None
 
 class DrawingArea(QWidget):
     def __init__(self):
         super().__init__()
         self.shapes = []
+        self.click_points = []
         self.current_shape = None
         self.shape_type = 'line'
         self.transformation_mode = None
+        self.clipping_mode = None
         self.selected_shape = None
         self.last_mouse_pos = None
-        self.setMouseTracking(True)
+        self.clipped_shapes = []
+        self.clip_window = None 
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -76,19 +160,35 @@ class DrawingArea(QWidget):
         pen.setWidth(2)
         painter.setPen(pen)
         
-        for shape in self.shapes:
-            shape.draw(painter)
+        if(self.clip_window):
+            for shape in self.clipped_shapes:
+                shape.draw(painter)
+        else:
+            for shape in self.shapes:
+                shape.draw(painter)
+        self.update()
 
     def mousePressEvent(self, event):
         if event.y() < 30:
             return
 
         if self.transformation_mode:
+            print(self.transformation_mode)
             if(not self.selected_shape):
                 self.selected_shape = self.get_shape_at(event.pos())
                 self.last_mouse_pos = event.pos()
             else:
                 self.selected_shape = None
+        elif self.clipping_mode == "cohen":
+            self.click_points.append(QPoint(event.x(), event.y()))
+            if len(self.click_points) == 2:
+                x_min = min(self.click_points[0].x(), self.click_points[1].x())
+                y_min = min(self.click_points[0].y(), self.click_points[1].y())
+                x_max = max(self.click_points[0].x(), self.click_points[1].x())
+                y_max = max(self.click_points[0].y(), self.click_points[1].y())
+                self.clip_window = CohenSutherland(x_min, y_min, x_max, y_max)
+                self.apply_clipping()
+                self.click_points.clear()
         else:
             if not self.current_shape:
                 if self.shape_type == 'line':
@@ -124,3 +224,12 @@ class DrawingArea(QWidget):
             if shape.contains(pos):
                 return shape
         return None
+
+    def apply_clipping(self):
+        if self.clip_window:
+            for shape in self.shapes:
+                if isinstance(shape, Line):
+                    clipped_line = self.clip_window.clip_line(shape)
+                    if clipped_line:
+                        self.clipped_shapes.append(clipped_line)
+        self.update()
